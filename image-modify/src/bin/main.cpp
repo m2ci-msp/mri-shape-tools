b85deab640a00a260113adb5346d0a90cd957c1c
Alexander Hewer <hewer@coli.uni-saarland.de>
2017-12-16 18:33:19 +0100

    work on image-modify tool

---
+++ image-modify/src/bin/main.cpp
@@ -0,0 +1,17 @@
+#include "settings.h"
+#include "ApplyModifications.h"
+
+int main(int argc, char* argv[]) {
+
+    Settings settings(argc, argv);
+
+    Image image;
+    image.read().from(settings.input);
+
+    ApplyModifications(image).apply(settings.specifications);
+
+    image.write().to(settings.output);
+
+    return 0;
+
+}
