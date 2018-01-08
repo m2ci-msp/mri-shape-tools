026ade862b937ef79e0ac86a0146a58ecce1e773
Alexander Hewer <hewer@coli.uni-saarland.de>
2018-01-08 15:50:44 +0100

    ProjectMesh uses new image classes now

--- shared_include/visualize/ProjectMesh.h
+++ shared_include/visualize/ProjectMesh.h
@@ -1,109 +1,109 @@
 #ifndef __PROJECT_MESH_H__
 #define __PROJECT_MESH_H__
 
+#include <armadillo>
+
 #include "mesh/Mesh.h"
-#include "scan/Scan.h"
+#include "image/Image.h"
 
 class ProjectMesh{
 
-  public:
+public:
 
-    ProjectMesh(const Mesh& mesh, const Scan& scan, int sampleAmount = 5) :
-      mesh(mesh), scan(scan), sampleAmount(sampleAmount) {
+  ProjectMesh(const Mesh& mesh, Image& scan, int sampleAmount = 5) :
+    mesh(mesh), scan(scan), sampleAmount(sampleAmount) {
 
-        const double hx = scan.data()->get_hx();
-        const double hy = scan.data()->get_hy();
-        const double hz = scan.data()->get_hz();
+    this->max = this->scan.extrema().get_max();
 
-        this->scanOrigin = arma::vec( {
-            scan.data()->get_org_x() * hx,
-            scan.data()->get_org_y() * hy,
-            scan.data()->get_org_z() * hz
-            });
+    this->scanOrigin = arma::vec( {
+        scan.info().get_origin_x(),
+          scan.info().get_origin_y(),
+          scan.info().get_origin_z()
+          });
 
-      }
+  }
 
-    /*------------------------------------------------------------------------*/
+  /*------------------------------------------------------------------------*/
 
-    Scan get_projected_mesh() {
+  Image get_projected_mesh() {
 
-      project_mesh_to_scan();
-      return this->scan;
+    project_mesh_to_scan();
+    return this->scan;
 
+  }
+
+  /*------------------------------------------------------------------------*/
+
+private:
+
+  /*------------------------------------------------------------------------*/
+
+  void project_mesh_to_scan() {
+
+    for( const auto& face: this->mesh.get_faces() ) {
+      draw_face(face);
     }
 
-    /*------------------------------------------------------------------------*/
+  }
 
-  private:
+  /*------------------------------------------------------------------------*/
 
-    /*------------------------------------------------------------------------*/
+  void draw_face(const std::vector<unsigned int>& face) {
 
-    void project_mesh_to_scan() {
+    // iterate over the edges between the vertices
+    for( size_t i = 0; i < face.size() - 1; ++i) {
 
-      for( const auto& face: this->mesh.get_faces() ) {
-        draw_face(face);
-      }
-
-    }
-
-    /*------------------------------------------------------------------------*/
-
-    void draw_face(const std::vector<unsigned int>& face) {
-
-      // iterate over the edges between the vertices
-      for( size_t i = 0; i < face.size() - 1; ++i) {
-
-        arma::vec start = this->mesh.get_vertices().at(face.at(i    ));
-        arma::vec end   = this->mesh.get_vertices().at(face.at(i + 1));
-
-        draw_edge(start, end);
-
-      }
-
-      // create edge between starting vertex and end vertex 
-      arma::vec start = this->mesh.get_vertices().at(face.at(0    ));
-      arma::vec end   = this->mesh.get_vertices().at(face.at(face.size() - 1));
+      arma::vec start = this->mesh.get_vertices().at(face.at(i    ));
+      arma::vec end   = this->mesh.get_vertices().at(face.at(i + 1));
 
       draw_edge(start, end);
 
     }
 
-    /*------------------------------------------------------------------------*/
+    // create edge between starting vertex and end vertex 
+    arma::vec start = this->mesh.get_vertices().at(face.at(0    ));
+    arma::vec end   = this->mesh.get_vertices().at(face.at(face.size() - 1));
 
-    void draw_edge(arma::vec start, arma::vec end) {
+    draw_edge(start, end);
 
-      // compute normalized direction vector representing the edge
-      const arma::vec direction = arma::normalise(end - start);
+  }
 
-      // derive sample distance on edge
-      const double sampleDistance =
-        arma::norm(end - start) / this->sampleAmount;
+  /*------------------------------------------------------------------------*/
 
-      // sample edge
-      for(int i = 0; i <= this->sampleAmount; ++i) {
+  void draw_edge(arma::vec start, arma::vec end) {
 
-        // sample point on edge and shift to origin of scan
-        arma::vec coordinate =
-          start + i * sampleDistance * direction - this->scanOrigin;
+    // compute normalized direction vector representing the edge
+    const arma::vec direction = arma::normalise(end - start);
 
-        // set voxel color to maximum at computed coordinate
-        this->scan.access()->set_value_coordinate(
-            coordinate,
-            this->scan.data()->get_max()
-            );
-      } // end for
+    // derive sample distance on edge
+    const double sampleDistance =
+      arma::norm(end - start) / this->sampleAmount;
 
-    } // end draw_edge
+    // sample edge
+    for(int i = 0; i <= this->sampleAmount; ++i) {
 
-    /*------------------------------------------------------------------------*/
+      // sample point on edge and shift to origin of scan
+      arma::vec coordinate =
+        start + i * sampleDistance * direction - this->scanOrigin;
 
-    Mesh mesh;
-    Scan scan;
+      // set voxel color to maximum at computed coordinate
+      this->scan.access().at_coordinate(coordinate(0), coordinate(1), coordinate(2) ) = this->max;
 
-    double sampleAmount;
+    } // end for
 
-    arma::vec scanOrigin;
+  } // end draw_edge
 
     /*------------------------------------------------------------------------*/
+
+  Mesh mesh;
+  Image scan;
+
+  double max;
+  double sampleAmount;
+
+  arma::vec scanOrigin;
+
+  /*------------------------------------------------------------------------*/
+
 };
 #endif
