02635102c5d3a8601de99aa0692cf2bd60d688b2
Alexander Hewer <hewer@coli.uni-saarland.de>
2018-01-08 14:06:38 +0100

    switched to new image classes

--- adaptive-threshold/src/bin/main.cpp
+++ adaptive-threshold/src/bin/main.cpp
@@ -2,10 +2,8 @@
 #include <vector>
 
 #include "settings.h"
-#include "scan/ScanIO.h"
+#include "image/Image.h"
 #include "mesh/MeshIO.h"
-
-#include "EnergyFunction.h"
 
 #include "CreatePointCloud.h"
 
@@ -13,33 +11,12 @@ int main(int argc, char* argv[]) {
 
   Settings settings(argc, argv);
 
-  auto source = ScanIO::read_from(settings.source);
+  Image source;
 
-  source.transform()->scale_values(0, 255);
-  source.transform()->round();
+  source.read().from(settings.source);
+  source.segment().otsu();
 
-  EnergyFunction function(source);
-
-  double minEnergy = DBL_MAX;
-  double bestThreshold = 0;
-
-  for(int t = 1; t < 255; ++t) {
-
-    const double currentEnergy = function.get_energy(t);
-    if( currentEnergy < minEnergy) {
-      minEnergy = currentEnergy;
-      bestThreshold = t;
-    }
-
-
-  }
-
-  std::cout << bestThreshold << std::endl;
-  std::cout << minEnergy << std::endl;
-
-  source.transform()->threshold(bestThreshold);
-
-  ScanIO::write_to(settings.output, source);
+  source.write().to(settings.output);
 
   // output point cloud if wanted
   if(settings.createCloud == true) {
