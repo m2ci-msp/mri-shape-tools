b002f423cf94dcc11d267ef0fc64666cf6cf50b6
Alexander Hewer <hewer@coli.uni-saarland.de>
2018-01-08 16:20:44 +0100

    adapted MeshColorizer to use new image classes

--- shared_include/mesh/MeshColorizer.h
+++ shared_include/mesh/MeshColorizer.h
@@ -2,14 +2,14 @@
 #define __MESH_COLORIZER_H__
 
 #include "mesh/Mesh.h"
-#include "scan/Scan.h"
+#include "image/Image.h"
 
 class MeshColorizer {
 
   public:
 
     /* color the vertices by using the values of a volume scan */
-    static void colorize(Mesh& mesh, const Scan& scan) {
+    static void colorize(Mesh& mesh, const Image& scan) {
 
       auto& colors = mesh.get_vertex_colors();
       // erase old colors
@@ -19,8 +19,10 @@ class MeshColorizer {
 
       // color the vertex according to the scan voxel it is located in
       for( const arma::vec& vertex: vertices) {
-        const double color = scan.get_value(vertex(0), vertex(1), vertex(2));
+
+        const double& color = scan.access().at_coordinate(vertex(0), vertex(1), vertex(2));
         colors.push_back(arma::vec({color, color, color}));
+
       }
 
     }
