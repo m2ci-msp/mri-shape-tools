6001b0c55b13a21ca433db1c429a3939346bc340
Alexander Hewer <hewer@coli.uni-saarland.de>
2018-01-24 11:17:53 +0100

    moved files

--- landmark-tool/src/include/action/ApplySegmentation.h
+++ landmark-tool/src/include/action/ApplySegmentation.h
@@ -5,7 +5,7 @@
 #include "core/SegmentationStackBuilder.h"
 #include "singleton/Data.h"
 #include "singleton/GuiDescription.h"
-#include "core/ApplyModifications.h"
+#include "image-modify/ApplyModifications.h"
 
 class ApplySegmentation : public Action {
 
