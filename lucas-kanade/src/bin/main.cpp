47c62be2e3f97ff5e6a5a697fe792129390fa8fc
Alexander Hewer <hewer@coli.uni-saarland.de>
2018-04-04 13:55:49 +0200

    further work

--- lucas-kanade/src/bin/main.cpp
+++ lucas-kanade/src/bin/main.cpp
@@ -11,25 +11,31 @@
 #include "energy/BuildTemplateFromMeshProfiles.h"
 #include "energy/OriginalTemplate.h"
 
+#include "energy/LandmarkReader.h"
+#include "energy/BuildBoundingBox.h"
+#include "energy/BuildTemplateFromBoundingBox.h"
+
 int main(int argc, char* argv[]) {
 
   Settings settings(argc, argv);
 
   Image templateImage, target;
 
-  Mesh mesh = MeshIO::read(settings.mesh);
-
-  const double offset = settings.offset;
-  const double profileLength = settings.profileLength;
-
   templateImage.read().from(settings.source);
   target.read().from(settings.target);
 
-  lucasKanade::BuildTemplateFromMeshProfiles builder(mesh, templateImage, offset, profileLength);
+  // read landmakrs
+  const std::vector<arma::vec> marks = LandmarkReader(settings.landmarkFile, templateImage).get();
 
-  // TODO: map to image domain needed?
-  const arma::vec& center = builder.get_center();
-  const std::vector<arma::vec>& originalLocations = builder.get_locations();
+  // build bounding box
+  arma::vec min, max;
+  BuildBoundingBox(marks).get(min, max);
+
+  // compute locations inside bounding box and center of box
+  lucasKanade::BuildTemplateFromBoundingBox builder(min, max, templateImage);
+
+  std::vector<arma::vec> originalLocations = builder.get_locations();
+  arma::vec center = builder.get_center();
 
   lucasKanade::EnergyData energyData(center, originalLocations, templateImage, target);
 
@@ -41,17 +47,17 @@ int main(int argc, char* argv[]) {
 
   Json::Value root(Json::objectValue);
 
-  root["tx"] = Json::Value(- energy.data().transformation[lucasKanade::TX] * templateImage.info().get_hx());
-  root["ty"] = Json::Value(- energy.data().transformation[lucasKanade::TY] * templateImage.info().get_hy());
-  root["tz"] = Json::Value(- energy.data().transformation[lucasKanade::TZ] * templateImage.info().get_hz());
+  root["tx"] = Json::Value( energy.data().transformation[lucasKanade::TX]);
+  root["ty"] = Json::Value( energy.data().transformation[lucasKanade::TY]);
+  root["tz"] = Json::Value( energy.data().transformation[lucasKanade::TZ]);
 
-  root["alpha"] = Json::Value( -energy.data().transformation[lucasKanade::ALPHA] );
-  root["beta"] = Json::Value( -energy.data().transformation[lucasKanade::BETA] );
-  root["gamma"] = Json::Value( -energy.data().transformation[lucasKanade::GAMMA] );
+  root["alpha"] = Json::Value( energy.data().transformation[lucasKanade::ALPHA] );
+  root["beta"] = Json::Value( energy.data().transformation[lucasKanade::BETA] );
+  root["gamma"] = Json::Value( energy.data().transformation[lucasKanade::GAMMA] );
 
-  root["ox"] = Json::Value( center(0) * templateImage.info().get_hx());
-  root["oy"] = Json::Value( center(1) * templateImage.info().get_hy());
-  root["oz"] = Json::Value( center(2) * templateImage.info().get_hz());
+  root["ox"] = Json::Value( center(0));
+  root["oy"] = Json::Value( center(1));
+  root["oz"] = Json::Value( center(2));
 
   std::ofstream outFile(settings.output);
 
