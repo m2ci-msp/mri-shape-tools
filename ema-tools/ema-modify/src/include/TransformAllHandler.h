f9b0f9692ddf6d269d3ae022473f51a3b2d047ca
Alexander Hewer <hewer@coli.uni-saarland.de>
2018-06-19 13:18:58 +0200

    ema-modify can now apply rotations to EMA data

--- ema-tools/ema-modify/src/include/TransformAllHandler.h
+++ ema-tools/ema-modify/src/include/TransformAllHandler.h
@@ -6,6 +6,8 @@
 #include "Handler.h"
 #include "ema/Ema.h"
 
+#include "matrix/Transformation.h"
+
 class EmaTransformAllHandler : public Handler {
 
 public:
@@ -16,7 +18,8 @@ public:
     const std::string type = options["type"].asString();
 
     if      ( type == "translate") { translate(ema, options); }
-    else if (type == "scale") { scale(ema, options); }
+    else if ( type == "scale") { scale(ema, options); }
+    else if ( type == "rotate") { rotate(ema, options) ;}
 
   }
 
@@ -35,6 +38,49 @@ public:
     const double factor = options["factor"].asDouble();
 
     ema.transform_all_coils().scale(factor);
+
+  }
+
+  void rotate(Ema& ema, Json::Value& options) {
+
+    // read axis
+    const std::string axis = options["axis"].asString();
+
+    // read angle
+    double angle = options["angle"].asDouble();
+
+    // convert angle to radians if needed
+    if( options["angleUnit"].empty() == false) {
+
+      if( options["angleUnit"].asString() == "degrees") {
+
+        angle = angle / 180 * M_PI;
+
+      }
+
+    }
+
+    // construct transformation depending on chosen rotation
+    // axis
+    Transformation transform;
+
+    if( axis == "x" ) {
+
+      transform = Transformation(angle, 0, 0);
+
+    }
+    else if ( axis == "y" ) {
+
+      transform = Transformation(0, angle, 0);
+
+    }
+    else if( axis == "z" ) {
+
+      transform = Transformation(0, 0, angle);
+
+    }
+
+    ema.transform_all_coils().apply_rigid_transformation(transform);
 
   }
 
